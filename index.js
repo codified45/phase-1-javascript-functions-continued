afac2e9165598440b7cda7e5609cd127b2a6b371
Jack Lach <jack45k@gmail.com>
2023-01-19 04:39:47 -0500

    commit functions continued lab - all 9 tests passing

--- index.js
+++ index.js
@@ -15,3 +15,35 @@ const mondayWork = function (mondayActivity = 'go to the office') {
     return mondayStatement;
 }
 
+// Replit:
+//  mondayWork
+// [Function: mondayWork]
+//  mondayWork(blastoff)
+// ReferenceError: blastoff is not defined
+//  mondayWork("blastoff")
+// 'This Monday, I will blastoff.'
+
+// function wrapAdjective(wrapType = '*'){
+//     //const wrappedAdjective;
+//     return function(wrappedAdjective = 'special', wrapType){
+//         const wrapResult = `You are ${wrapType}${wrappedAdjective}${wrapType}!`;
+
+//         return wrapResult;
+//     }
+// }
+
+function wrapAdjective(wrapType = '*'){
+    //const wrappedAdjective;
+    // const wrapTypeFunction = wrapAdjective("wrapType");
+    // none of this, only one parameter should be passed to inner function
+    return function(wrappedAdjective = 'special'){
+        const wrapResult = `You are ${wrapType}${wrappedAdjective}${wrapType}!`;
+
+        return wrapResult;
+    }
+}
+
+// Hint: hit control+c anytime to enter REPL.
+//  wrapAdjective("&&&")("a dedicated programmer")
+// 'You are &&&a dedicated programmer&&&!'
+//  
