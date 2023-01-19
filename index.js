f24ba827296c5cc2a20a379a3441cbd58f28b9c9
Jack Lach <jack45k@gmail.com>
2023-01-18 23:04:38 -0500

    commit functions continued lab - first 3 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function saturdayFun(funAction = 'roller-skate') {
+
+    let saturdayResponse;
+
+    saturdayResponse = `This Saturday, I want to ${funAction}!`;
+    return saturdayResponse;
+}
+
